ff6181d8e952859d3184ac7c92adbfe6da613d0d
Arisu Desu <arisu@arisu.me>
2017-02-28 22:40:09 +0300

    Update dotfiles

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,14 +1,7 @@
 files="x11/xprofile     .xprofile
        x11/xresources   .config/Xresources
        compton.conf     .config/compton.conf
-       tint2rc          .config/tint2/tint2rc"
-
-files="${files}
-       openbox/menu.xml          .config/openbox/menu.xml
-       openbox/menu.sh           .config/openbox/menu.sh
-       openbox/menu-functions.sh .config/openbox/menu-functions.sh
-       openbox/menu-randr.sh     .config/openbox/menu-randr.sh
-       openbox/autostart         .config/openbox/autostart"
+       awesome/rc.lua   .config/awesome/rc.lua"
 
 # ---------------------------------------
 
