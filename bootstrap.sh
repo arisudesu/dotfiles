5b55d1d73571b1972b9a60f0c01503ae83468174
Arisu Desu <arisu@arisu.me>
2017-05-08 00:03:19 +0300

    Update dotfiles

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,8 +2,13 @@
 
 FILES=("x11/xprofile          $HOME/.xprofile"
        "x11/xresources        $HOME/.config/Xresources"
+
        "gtk-2.0/gtkrc         $HOME/.config/gtk-2.0/gtkrc"
        "gtk-3.0/settings.ini  $HOME/.config/gtk-3.0/settings.ini"
+
+       "qt4/Trolltech.conf    $HOME/.config/Trolltech.conf"
+       "qt5/qt5ct.conf        $HOME/.config/qt5ct/qt5ct.conf"
+
        "compton.conf          $HOME/.config/compton.conf"
        "awesome/rc.lua        $HOME/.config/awesome/rc.lua"
        "nanorc                $HOME/.nanorc")
